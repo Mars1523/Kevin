af1062d9f519c80db64dce28ed3a78eff7c2923c
Noskcaj <kcajdev@gmail.com>
2019-01-31 13:50:08 -0500

    Implement initial encoder support for the sim

--- robot/physics.py
+++ robot/physics.py
@@ -1,6 +1,9 @@
 from pyfrc.physics import drivetrains
+import math
 
-ENCODER_REVOLUTION = 360
+ENCODER_REVOLUTION = 1024
+
+ENCODER_TICKS = ENCODER_REVOLUTION / (0.5 * math.pi)
 
 
 class PhysicsEngine:
@@ -10,6 +13,7 @@ class PhysicsEngine:
                                    to communicate simulation effects to
         """
 
+        self.fl_encoder = self.fr_encoder = self.rl_encoder = self.rr_encoder = 0
         self.physics_controller = physics_controller
 
         """
@@ -46,8 +50,21 @@ class PhysicsEngine:
                 rear_l, rear_r, front_l, front_r
             )
             self.physics_controller.drive(speed, rotation, tm_diff)
+            self.fl_encoder += self.tank_drivetrain.l_speed * tm_diff * 10
+            self.fr_encoder += self.tank_drivetrain.r_speed * tm_diff * 10
+            self.rl_encoder += self.tank_drivetrain.l_speed * tm_diff * 10
+            self.rr_encoder += self.tank_drivetrain.r_speed * tm_diff * 10
+
         else:
             x_speed, y_speed, rotation = self.mecaum_drivetrain.get_vector(
                 rear_l, rear_r, front_l, front_r
             )
             self.physics_controller.vector_drive(x_speed, y_speed, rotation, tm_diff)
+            self.fl_encoder += self.mecaum_drivetrain.lf_speed * tm_diff * 1000
+            self.fr_encoder += self.mecaum_drivetrain.rf_speed * tm_diff * 1000
+            self.rl_encoder += self.mecaum_drivetrain.lr_speed * tm_diff * 1000
+            self.rr_encoder += self.mecaum_drivetrain.rr_speed * tm_diff * 1000
+        hal_data["CAN"][2]["quad_position"] = int(self.fl_encoder * ENCODER_TICKS)
+        hal_data["CAN"][4]["quad_position"] = int(self.fr_encoder * ENCODER_TICKS)
+        hal_data["CAN"][3]["quad_position"] = int(self.rl_encoder * ENCODER_TICKS)
+        hal_data["CAN"][5]["quad_position"] = int(self.rr_encoder * ENCODER_TICKS)
