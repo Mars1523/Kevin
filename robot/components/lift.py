369826936cbd81278a3c4a810741dde4a222c63d
Noskcaj <kcajdev@gmail.com>
2019-02-12 13:53:23 -0500

    Implement lift max limit

--- robot/components/lift.py
+++ robot/components/lift.py
@@ -3,6 +3,9 @@ import wpilib
 from common.encoder import BaseEncoder
 
 
+LIFT_ENCODER_MAX = 2170
+
+
 class Lift:
     """
         A position PID enabled lift with preset heights
@@ -21,13 +24,12 @@ class Lift:
         self.pid_controller.setContinuous(False)
         self.pid_controller.setOutputRange(-0.35, 0.85)
         self.pid_controller.enable()
-        self.pid_controller.setSetpoint(0)
 
     def set_speed(self, speed):
         self.speed = speed
 
     def set_setpoint(self, setpoint):
-        self.setpoint = max(0, setpoint)
+        self.setpoint = max(0, min(setpoint, LIFT_ENCODER_MAX))
 
     def get_setpoint(self):
         return self.pid_controller.getSetpoint()
