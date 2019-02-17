d39c6a35904465c135834097d0b27caf3f769b78
Noskcaj <kcajdev@gmail.com>
2019-02-16 23:34:48 -0500

    Add PID to cargo aligner

--- robot/controllers/align_cargo.py
+++ robot/controllers/align_cargo.py
@@ -1,6 +1,25 @@
+import wpilib
+import wpilib.interfaces
+
 from networktables.entry import NetworkTableEntry
 
 from components.drive import Drive, DriveMode
+
+
+class PID(wpilib.interfaces.PIDSource, wpilib.interfaces.PIDOutput):
+    output = 0.0
+
+    def __init__(self, yaw):
+        self.yaw = yaw
+
+    def getPIDSourceType(self) -> wpilib.interfaces.PIDSource.PIDSourceType:
+        return wpilib.interfaces.PIDSource.PIDSourceType.kDisplacement
+
+    def pidGet(self) -> float:
+        return self.yaw.getNumber(0.0)
+
+    def pidWrite(self, output: float) -> None:
+        self.output = output
 
 
 class AlignCargo:
@@ -9,19 +28,28 @@ class AlignCargo:
 
     def setup(self):
         self.enabled = False
+        self.pid_source = PID(self.cargo_yaw)
+        self.pid = wpilib.PIDController(
+            0.045, 0.0002, 0.04, 0, self.pid_source, self.pid_source
+        )
+        self.pid.setAbsoluteTolerance(5)
+        self.pid.setContinuous(False)
+        self.pid.setOutputRange(-0.7, 0.7)
+        self.pid.enable()
+        self.output = 0
+        self.on_target = False
 
     def set_enabled(self, enabled):
         self.enabled = enabled
+        # if enabled:
+        #     self.on_target = False
 
     def execute(self):
-        if self.enabled:
-            yaw = self.cargo_yaw.getNumber(0)
-            self.drive.set_mode(DriveMode.MECANUM)
-            if yaw > 1:
-                self.drive.drive_mecanum(0.24, 0.24, 0, adjusted=False)
-                # pass
-            elif yaw < -1:
-                # pass
-                self.drive.drive_mecanum(-0.24, 0.24, 0, adjusted=False)
-            else:
-                self.drive.drive_mecanum(0, 0, 0, adjusted=False)
+        forward = 0.55 if self.on_target else 0
+        self.pid.setEnabled(self.enabled)
+        self.drive.set_mode(DriveMode.MECANUM)
+        self.drive.drive_mecanum(-self.pid_source.output, forward, 0)
+        yaw = abs(self.pid_source.yaw.getNumber(0))
+        # print(self.pid_source.output, self.on_target, yaw)
+        self.on_target = yaw < 4
+        print("seek", self.on_target)
