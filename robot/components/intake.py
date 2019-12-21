b73fc3e650362d3fcb4d23d515e94e0977b3452c
Noskcaj <kcajdev@gmail.com>
2019-12-21 17:32:08 -0500

    Comment on intake.py

--- robot/components/intake.py
+++ robot/components/intake.py
@@ -30,20 +30,43 @@ class Intake:
         self.extend = False
         self.grab = False
 
+        ## This is a PID controller (which is a kind of closed loop control)
+        ## which takes input from the wrist encoder, and uses that to control the
+        ## wrist motor and hold it at a certain rotation.
+        ##
+        ## https://frc-pdr.readthedocs.io/en/latest/control/using_WPILIB's_pid_controller.html
+        ##
+        ## Interestingly, this is technically refered to a "P Loop", because
+        ## there is only a "proportion" value, no I or D.
         self.pid_controller = wpilib.PIDController(
             0.0256, 0.0, 0.0, self.wrist_encoder, self.wrist_motor
         )
+        ## Set the tolerance (this is only actually useful if you use the .onTarget()
+        ## functions).  However, PID can be an arcane magic, and this code works.
         self.pid_controller.setAbsoluteTolerance(0.5)
+        ## We do not want to the wrist to go backwards into the robot, so
+        ## we tell the controller it is not a continuous circle.
         self.pid_controller.setContinuous(False)
+        ## The motors accept speeds in a range of -1.0 to 1.0, so we tell the
+        ## controller that is the range of values we want
         self.pid_controller.setOutputRange(-1, 1)
+        ## Set the pid controller starting position so the pid controller does
+        ## not try to send the wrist to zero degrees
         self.pid_controller.setSetpoint(self.wrist_setpoint)
         # 136-215-220 (changed) 281-208-270
+        ## Actually turn on the controller.  Unless you call this, they won't do
+        ## anything.  I have lost too much of my life to disabled PID controllers.
         self.pid_controller.enable()
 
     def set_speed(self, speed):
         self.speed = speed
 
     def set_wrist_setpoint(self, setpoint):
+        ## This max(x, min(y, z)) thing ensures that the setpoint value never
+        ## exeeds a minimum or maximum value, because that causes things to break.
+        ##
+        ## Note that the order of constant values here is _very_ important and
+        ## the incorrect order will cause strange behavior.
         clamped = max(208, min(273, setpoint))
         self.wrist_setpoint = clamped
 
